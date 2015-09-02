1286fd2816cc2bfaf44d08d1ae408ec969af08e6
Jennifer <yortree@gmail.com>
2015-09-02 15:12:34 -0700

    Utilize jQuery using treehouse.json as example.

---
+++ treehousebadges.js
@@ -0,0 +1,20 @@
+// mycode.js
+var url = 'https://teamtreehouse.com/jenniferminetree.json';
+var icon_url = [];
+
+$.ajax(url).done(showBadges);
+ function showBadges(results){
+	 console.log(results);
+	 var myBadges = results.badges;
+	 for (var i = 0; i < myBadges.length; ++i){
+		 var img = $('<img>', { 'src': myBadges[i].icon_url  });
+		 img.attr('width', 100);
+		 img.attr('height', 100);
+		 img.appendTo('body');
+	 }
+ };
+
+
+//  var user = new badgeGrid('body', results){
+	// 	 user.render();
+	//  }
